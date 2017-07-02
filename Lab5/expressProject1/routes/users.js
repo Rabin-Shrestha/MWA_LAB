36ff4b5aa6a73a686db36934086667101d150168
RabinShrestha <lovingspirit617@gmail.com>
2017-07-02 17:17:40 -0500

    modified users.js

--- Lab5/expressProject1/routes/users.js
+++ Lab5/expressProject1/routes/users.js
@@ -10,7 +10,7 @@ router.get('/', function(req, res, next) {
     const dataFetchPromise = fetch('http://jsonplaceholder.typicode.com/users');
     dataFetchPromise.then(function (data1) {
         return data1.json();
-    }).then((data)=>{ res.render('users',{data})});
+    }).then((data)=>{ res.render('users',{users:data})});// send data to the users.jade file to render it
 
 
   //res.send('respond with a resource');

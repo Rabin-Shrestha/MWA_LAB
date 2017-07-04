771be0a9dd5af42dd49a3ccc6cdd018ee0ba1c08
RabinShrestha <lovingspirit617@gmail.com>
2017-07-04 09:53:53 -0500

    Reaading ReadDatabase table and decrypting the message received and showing to browser done

--- Lab7/ReadDatabase/app.js
+++ Lab7/ReadDatabase/app.js
@@ -48,7 +48,6 @@ app.use(function (err, req, res, next) {
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
-
     // render the error page
     res.status(err.status || 500);
     res.render('error');
@@ -56,4 +55,5 @@ app.use(function (err, req, res, next) {
 
 db.close();
 module.exports = app;
-app.listen(8585);
+
+app.listen(8080);
